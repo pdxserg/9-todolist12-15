252317801045d5253766a25d84395a03a39dba9a
SergeyStarchenko <pdxserg@gmail.com>
2024-12-18 11:21:38 -0800

    refact...

--- src/model/theme-reducer.tsx
+++ src/model/theme-reducer.tsx
@@ -1,9 +1,15 @@
 // @flow
 
 
-import {ThemeMode} from "../App";
 
-export const themeReducer=(state:ThemeMode, action:ActionsType):ThemeMode=>{
+
+type ThemeMode = "dark"|"light"
+
+const initialState = {
+
+}
+
+export const themeReducer=(state:any , action:ActionsType):ThemeMode=>{
 switch (action.type) {
 	case 'CHANGE_MODE':{
 	return state
