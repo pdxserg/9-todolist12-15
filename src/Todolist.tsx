85c45c8ef5c3ab24c4f61eff749c5928e97c6e3e
SergeyStarchenko <pdxserg@gmail.com>
2024-12-22 21:21:03 -0800

    refact...

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -9,6 +9,7 @@ import {changeTodolistTitleAC, removeTodolistAC, TodolistType} from "./model/tod
 import {FilterTasksButtons} from "./FilterTasksButtons";
 import {useDispatch} from "react-redux";
 import {Tasks} from "./Tasks";
+import {TodolistTitle} from "./TodolistTitle";
 
 
 type PropsType = {
@@ -19,25 +20,13 @@ export const Todolist = ({todo}: PropsType) => {
 	const todolistId = todo.id
 	const dispatch = useDispatch()
 
-	const removeTodolistHandler = () => {
-		dispatch(removeTodolistAC(todolistId))
-	}
 	const addTaskCallback = (title: string) => {
 		dispatch(addTaskAC({title, todolistId}))
 	}
-	const updateTodolistHandler = (title: string) => {
-		dispatch(changeTodolistTitleAC(todolistId, title))
-	}
-
 
 	return (
 		<div>
-			<div className={"todolist-title-container"}>
-				<h3><EditableSpan value={todo.title} onChange={updateTodolistHandler}/></h3>
-				<IconButton onClick={removeTodolistHandler}>
-					<DeleteIcon/>
-				</IconButton>
-			</div>
+			<TodolistTitle todo={todo}/>
 			<AddItemForm addItem={addTaskCallback}/>
 			<Tasks todo={todo}/>
 			<Box sx={filterButtonsContainerSx}>
---
+++ src/TodolistTitle.tsx
@@ -0,0 +1,31 @@
+// @flow
+import * as React from 'react';
+import {EditableSpan} from "./common/EditableSpan";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {changeTodolistTitleAC, removeTodolistAC, TodolistType} from "./model/todolists-reducer";
+import {useDispatch} from "react-redux";
+
+type Props = {
+	todo: TodolistType,
+};
+export const TodolistTitle = ({todo}: Props) => {
+	const dispatch = useDispatch()
+
+
+	const removeTodolistHandler = () => {
+		dispatch(removeTodolistAC(todo.id))
+	}
+
+	const updateTodolistHandler = (title: string) => {
+		dispatch(changeTodolistTitleAC(todo.id, title))
+	}
+	return (
+		<div className={"todolist-title-container"}>
+			<h3><EditableSpan value={todo.title} onChange={updateTodolistHandler}/></h3>
+			<IconButton onClick={removeTodolistHandler}>
+				<DeleteIcon/>
+			</IconButton>
+		</div>
+	);
+};
