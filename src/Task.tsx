d6264c198112bf23624ccbe6bb5e25b383c1d1b9
SergeyStarchenko <pdxserg@gmail.com>
2024-12-22 21:38:27 -0800

    refact...

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,13 +1,59 @@
 // @flow 
 import * as React from 'react';
+import {ChangeEvent} from "react";
+import ListItem from "@mui/material/ListItem";
+import {getListItemSx} from "./Todolist.styles";
+import Checkbox from "@mui/material/Checkbox";
+import {EditableSpan} from "./common/EditableSpan";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {TodolistType} from "./model/todolists-reducer";
+import {useDispatch} from "react-redux";
+import {changeStatusTaskAC, removeTaskAC, TaskType, updateTaskAC} from "./model/tasks-reducer";
 
 type Props = {
-	
+	todo:TodolistType,
+	tasksForTodolist:TaskType[]
 };
-export const Task = (props: Props) => {
+export const Task = ({tasksForTodolist,todo }: Props) => {
+
+	const dispatch = useDispatch()
+
+	const removeTask = (taskId: string, todolistId: string) => {
+		dispatch(removeTaskAC({taskId, todolistId}))
+	}
+	const changeTaskStatus = (taskId: string, status: boolean, todolistId: string) => {
+		dispatch(changeStatusTaskAC({taskId, todolistId, status}))
+	}
+	const updateTask = (todolistId: string, taskId: string, title: string) => {
+		dispatch(updateTaskAC({taskId, todolistId, title}))
+	}
 	return (
 		<div>
+			{tasksForTodolist.map((task) => {
 
+				const removeTaskHandler = () => {
+					removeTask(task.id, todo.id)
+				}
+
+				const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
+					const newStatusValue = e.currentTarget.checked
+					changeTaskStatus(task.id, newStatusValue, todo.id)
+				}
+
+				const changeTaskTitleHandler = (title: string) => {
+					updateTask(todo.id, task.id, title)
+				}
+				return <ListItem key={task.id} sx={getListItemSx(task.isDone)}>
+					<div>
+						<Checkbox checked={task.isDone} onChange={changeTaskStatusHandler}/>
+						<EditableSpan value={task.title} onChange={changeTaskTitleHandler}/>
+					</div>
+					<IconButton onClick={removeTaskHandler}>
+						<DeleteIcon/>
+					</IconButton>
+				</ListItem>
+			})}
 		</div>
 	);
 };
--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -17,23 +17,16 @@ import {
 import {useDispatch, useSelector} from 'react-redux';
 import {TodolistType} from "./model/todolists-reducer";
 import {RootStateType} from "./store";
+import {Task} from "./Task";
 
 type Props ={
 	todo:TodolistType,
 };
 export const Tasks = ({ todo}: Props) => {
 	const tasks = useSelector<RootStateType, TasksStateType>(state => state.tasks)
-	const dispatch = useDispatch()
 
-	const removeTask = (taskId: string, todolistId: string) => {
-		dispatch(removeTaskAC({taskId, todolistId}))
-	}
-	const changeTaskStatus = (taskId: string, status: boolean, todolistId: string) => {
-		dispatch(changeStatusTaskAC({taskId, todolistId, status}))
-	}
-	const updateTask = (todolistId: string, taskId: string, title: string) => {
-		dispatch(updateTaskAC({taskId, todolistId, title}))
-	}
+
+
 	const allTodolistTasks = tasks[todo.id]
 	let tasksForTodolist = allTodolistTasks
 
@@ -51,30 +44,7 @@ export const Tasks = ({ todo}: Props) => {
 				tasksForTodolist.length === 0
 					? <p>Тасок нет</p>
 					: <List>
-						{tasksForTodolist.map((task) => {
-
-							const removeTaskHandler = () => {
-								removeTask(task.id, todo.id)
-							}
-
-							const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
-								const newStatusValue = e.currentTarget.checked
-								changeTaskStatus(task.id, newStatusValue, todo.id)
-							}
-
-							const changeTaskTitleHandler = (title: string) => {
-								updateTask(todo.id, task.id, title)
-							}
-							return <ListItem key={task.id} sx={getListItemSx(task.isDone)}>
-								<div>
-									<Checkbox checked={task.isDone} onChange={changeTaskStatusHandler}/>
-									<EditableSpan value={task.title} onChange={changeTaskTitleHandler}/>
-								</div>
-								<IconButton onClick={removeTaskHandler}>
-									<DeleteIcon/>
-								</IconButton>
-							</ListItem>
-						})}
+						<Task  todo={todo} tasksForTodolist={tasksForTodolist}/>
 					</List>
 			}
 
