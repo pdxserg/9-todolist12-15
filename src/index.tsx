6dc0f5f7952a56ccf1facc40400b3f50a6e9956a
SergeyStarchenko <pdxserg@gmail.com>
2024-12-28 18:22:36 -0800

    refact...

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './app/App';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
+import {AppHttpRequests} from "./app/AppHttpRequests";
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -11,7 +12,8 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		<Provider store={ store}>
-			<App/>
+			{/*<App/>*/}
+			<AppHttpRequests/>
 		</Provider>
 	</React.StrictMode>
 );
