0b93b9c4abaae215113c1778a2fab2cbeecee758
SergeyStarchenko <pdxserg@gmail.com>
2025-01-12 14:23:54 -0800

    refact...

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,34 +1,21 @@
 import axios from "axios";
 import {Respond, TodolistsType} from "./todolistsApi.types";
 import {headersToken} from "../../../app/token/token";
+import {instance} from "../../../common/instance/instance";
 
 export const todolistsApi={
 	 getTodolists: ()=>{
-		 return axios.get<TodolistsType>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
-			headers: headersToken
-		})
+		 return instance.get<TodolistsType>('/todo-lists')
 	},
 	createTodolists: (title:string)=>{
-		return axios
-			.post<Respond<{item: TodolistsType}>>(
-				'https://social-network.samuraijs.com/api/1.1/todo-lists',
-				{title}, {headers: headersToken})
+		return instance.post<Respond<{item: TodolistsType}>>('/todo-lists', {title})
 	},
 	deleteTodolist:(id:string)=>{
-	return 	axios
-			.delete<Respond>(
-				`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`,
-				{headers: headersToken}
-			)
+	return 	instance.delete<Respond>(`/${id}`)
 	},
 	updateTodolist:(arg:{id: string, title: string})=>{
 		 const {id,title}=arg
-		return axios
-			.put<any>(
-				`https://social-network.samuraijs.com/api/1.1/todo-lists/${id}`,
-				{title},
-				{headers: headersToken}
-			)
+		return instance.put<any>(`/${id}`, {title})
 	}
 
 
