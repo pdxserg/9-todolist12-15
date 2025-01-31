305b11312065112e78b0986fa375eb41a076859d
SergeyStarchenko <pdxserg@gmail.com>
2025-01-30 21:45:40 -0800

    refact...4_4 auth

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,8 +1,9 @@
 import { Inputs } from "../ui/Login/Login"
 import { instance } from "../../../common/instance"
+import { Respond } from "../../../common/types/types"
 
 export const authApi = {
-  login: (arg: { Inputs }) => {
-    return instance.post("/auth/login")
+  login: (args: Inputs) => {
+    return instance.post<Respond<{ userId: string }>>("/auth/login", args)
   },
 }
